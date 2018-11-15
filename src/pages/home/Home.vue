9fa25afa5322057dda1fd94f6f8818696ccf88e0
liuxu <920447564@qq.com>
2018-11-15 18:51:10 +0800

    Initial commit

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HomeHeader></HomeHeader>
+  </div>
+
+</template>
+
+<script>
+  import HomeHeader from './components/Header'
+    export default {
+        name: "home.vue",
+      components:{HomeHeader}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">输入城市/景点/游玩主题</div>
+    <div class="header-right">城市</div>
+  </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style lang="stylus" scoped>
+.header
+  display:flex
+  line-height:0.86rem
+  background :#00bcd4
+  color: #fff
+  .header-left
+     width:.64rem
+     float:left
+  .header-input
+     flex:1
+     height:.64rem
+     line-height:.64rem
+     background :#fff
+     border-radius:.1rem
+     margin-top :.12rem
+     margin-left:.2rem
+     color:#ccc
+  .header-right
+      width:1.24rem
+      float:right
+      text-align center
+
+</style>
