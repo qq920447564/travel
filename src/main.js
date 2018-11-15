953c3b79630fc848a70d38edd365bacbf15230c1
liuxu <920447564@qq.com>
2018-11-15 20:59:50 +0800

    轮播图的实现其中有一个样式错误无法处理

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import fastClick from 'fastclick'
 import '@/assets/styles/reset.css'
 import '@/assets/styles/border.css'/*引入项目边框*/
 import '@/assets/styles/iconfont.css'/*全局引入图标文件*/
-
+import  VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <HomeHeader></HomeHeader>
+   <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 
 </template>
 
 <script>
   import HomeHeader from './components/Header'
+  import HomeSwiper from './components/Swiper'
     export default {
-        name: "home.vue",
-      components:{HomeHeader}
+        name: "Home",
+      components:{HomeHeader,HomeSwiper}
     }
 </script>
 
